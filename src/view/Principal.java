d4759879adb473a92cc010992f17615396d5f167
knn654 <kaua.nascimento@fatec.sp.gov.br>
2023-05-12 02:13:33 -0300

    Update

--- src/view/Principal.java
+++ src/view/Principal.java
@@ -5,6 +5,7 @@ import controller.ModificacaoCadastroController;
 public class Principal {
 	public static void main(String[] args) throws Exception {
 		ModificacaoCadastroController modCadastro = new ModificacaoCadastroController();
+		System.out.println("Iniciando geração de arquivos");
 		modCadastro.novoCadastro(41, 60, 8000.00);
 		modCadastro.novoCadastro(31, 40, 5000.00);
 		modCadastro.novoCadastro(21, 30, 3000.00);
